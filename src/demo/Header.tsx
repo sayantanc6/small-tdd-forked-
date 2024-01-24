1bcec25aa66bf6d5d75079d8f906025eb11a1e02
CodeSandbox <support@codesandbox.io>
2024-01-24 10:47:26 +0000

    Initial commit

---
+++ src/demo/Header.tsx
@@ -0,0 +1,7 @@
+export function Header(props) {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Header } from "./demo/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header title="demo111" />
     <App />
   </React.StrictMode>,
 );
